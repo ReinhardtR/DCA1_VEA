8bc38c2c46082f642c665ffc50c6b2843eaa0ce7
Milo Madsen <miloamadsen@gmail.com>
2024-02-20 10:13:19 +0100

    Adding different test situation

--- Tests/VEA.Tests.UnitTests/OperationResult/OperationResultUnitTest.cs
+++ Tests/VEA.Tests.UnitTests/OperationResult/OperationResultUnitTest.cs
@@ -7,9 +7,34 @@ public class OperationResultUnitTest
     private record TestPayload(string Name, int Age);
 
     [Fact]
-    public void Test1()
+    public void Returns_Nothing_CannotFail()
     {
+        //Return Void?
         Result none = Result.Success();
         Result something = Result.Success(new TestPayload("John", 25));
+    }
+    
+    [Fact]
+    public void Returns_Nothing_CanFail()
+    {
+        //Return Result<Void>? With a possible error?
+    }
+    
+    [Fact]
+    public void Returns_Something_CannotFail()
+    {
+        //Return T?
+    }
+    
+    [Fact]
+    public void Returns_Something_CanFail()
+    {
+        //Return Result<T>? With a possible error?
+    }
+    
+    [Fact]
+    public void Returns_Something_CanFail_WithMultipleErrors()
+    {
+        //Return Result<T>? With a possible error?
     }
 }
