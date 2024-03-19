df5b6af94bb22ac2edf2c25c408e32d5bb26b259
Jakob Kjellerup <sorenkjellerup@gmail.com>
2024-03-19 11:01:32 +0100

    Event Repository interface

--- src/Core/VEA.Core.Domain/Aggregates/Events/IEventRepository.cs
+++ src/Core/VEA.Core.Domain/Aggregates/Events/IEventRepository.cs
@@ -1,6 +1,10 @@
-﻿namespace VEA.Core.Domain.Aggregates.Events;
+﻿using VEA.Core.Tools.OperationResult;
+
+namespace VEA.Core.Domain.Aggregates.Events;
 
 public interface IEventRepository
 {
-    
+    Task<Result> AddAsync(VeaEvent veaEvent);
+    Task<Result> FindAsync(EventId id);
+    Task<Result> RemoveAsync(EventId id);
 }
