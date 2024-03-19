b793c20219b3477a6df52972ef988774b2e28140
Jakob Kjellerup <sorenkjellerup@gmail.com>
2024-03-19 11:02:42 +0100

    Guest  repository

--- src/Core/VEA.Core.Domain/Aggregates/Guests/IGuestRepository.cs
+++ src/Core/VEA.Core.Domain/Aggregates/Guests/IGuestRepository.cs
@@ -1,6 +1,11 @@
-﻿namespace VEA.Core.Domain.Aggregates.Guests;
+﻿using VEA.Core.Domain.Aggregates.Events;
+using VEA.Core.Tools.OperationResult;
+
+namespace VEA.Core.Domain.Aggregates.Guests;
 
 public interface IGuestRepository
 {
-    
+    Task<Result> AddAsync(Guest guest);
+    Task<Result> FindAsync(GuestId id);
+    Task<Result> RemoveAsync(GuestId id);
 }
