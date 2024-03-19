92d2299115e1edf25947148656b67d8a14d87380
Jakob Kjellerup <sorenkjellerup@gmail.com>
2024-03-19 11:17:04 +0100

    Command interface made by copilot

---
+++ src/Core/VEA.Core.Application/AppEntry/ICommand.cs
@@ -0,0 +1,10 @@
+﻿namespace VEA.Core.Application.AppEntry;
+
+public abstract class ICommand
+{
+    // with only create method
+    public static T Create<T>() where T : ICommand, new()
+    {
+        return new T();
+    }
+}
